e0c981715ea2d58a85fd7fb4761869f39a04b461
ChiBQ2001 <buiquocchi12tn3@gmail.com>
2022-11-11 14:43:54 +0700

    add schedule page

--- wp-content/themes/suite/templates/template-schedule.php
+++ wp-content/themes/suite/templates/template-schedule.php
@@ -1,81 +1,68 @@
-<div>
+<div class="group-border">
     <?php 
         require_once(DIR_MODEL . 'model-schedule-function.php');
         $model = new Admin_Model_Schedule_Function();
         $data = $model->getAllDataSchedule();
         $tmp = array();
-        //print_r($data);
         if(!empty($data)){
             foreach ($data as $key => $arg) {
                 $tmp[$arg['month'] . ' / ' . $arg['year']][] = $arg['id'];  
-                $tmp[$arg['title']] = $arg['title'];
-                $tmp[$arg['weekdays']] = $arg['weekdays'];
-                $tmp[$arg['day']] = $arg['day'];
-                $tmp[$arg['month']] = $arg['month'];
-                $tmp[$arg['year']] = $arg['year'];
-                $tmp[$arg['time']] = $arg['time'];
-                $tmp[$arg['place']] = $arg['place'];
-                $tmp[$arg['note']] = $arg['note'];
             }
         }
-        //print_r($tmp);
+        //lay group month/year va cac data theo id
         $output = array();
         foreach($tmp as $type => $labels){
             $output[] = array(
                 'month' => $type,
                 'id' => $labels,
-                //'title', 'weekdays', 'day', 'time', 'note', 'place',
-                // 'title' => $labels,
-                // 'weekdays' => $labels,
-                // 'day' => $labels,
-                // 'time' => $labels,
-                // 'note' => $labels,
-                // 'place' => $labels
             );
         }
-        print_r($output);
+        //lay data theo group month/year
         $ids = array();
         foreach($output as $val){
             $ids = $val['id'];
-            //print_r($val['id']);
-            //lay thoi gian dau cuoi
-            if ($val['time']) {
-                $time = explode('-', $val['time']);
-                $vTimeStart = $time[0];
-                $vTimeEnd = $time[1];
-            } else {
-                $vTimeStart = $val['timeStart'];
-                $vTimeEnd = $val['timeEnd'];
+            ?><div class="group-title"><label><?php echo $val['month'] ?></label></div><?php
+            //dua theo ids, ta co dinh dang Array[0][column] => value
+            foreach($ids as $id){
+                $data1 = $model->getDataScheduleByID($id);
+                //lay thoi gian dau cuoi
+                if ($data1[0]['time']) {
+                    $time = explode('-', $data1[0]['time']);
+                    $vTimeStart = $time[0];
+                    $vTimeEnd = $time[1];
+                } else {
+                    $vTimeStart = $data1[0]['timeStart'];
+                    $vTimeEnd = $data1[0]['timeEnd'];
+                }
+                ?>
+                <div class="schedule-item">
+                    <div class="schedule-head">
+                        <div class="schedule-title">
+                            <div><?php echo $data1[0]['title'] ?></div>
+                            <div><i class="fas fa-angle-double-down"></i></div>
+                        </div>
+                        <div class="schedule-time">
+                            <?php echo $data1[0]['weekdays'] . ' ' . $data1[0]['day'] . '-' . $data1[0]['month'] . '-' . $data1[0]['year'] ?>
+                            &nbsp;&nbsp;
+                            <?php echo _e('Time') . ' : ' . $vTimeStart . ' -- ' . $vTimeEnd ?>
+                        </div>
+                    </div>
+                    <div class="schedule-content">
+                        <div><label><b><?php _e('Place') ?> :</b></label> <?php echo $data1[0]['place'] ?></div>
+                        <div><label><b><?php _e('Note') ?> :</b></label> <?php echo $data1[0]['note'] ?></div>
+                    </div>
+                </div>
+                <?php
             }
-            
-            ?>
-            <div class="schedule-month"><?php echo $val['month'] ?></div>
-            <div class="schedule-item">
-                <div class="schedule-head">
-                    <div class="schedule-title">
-                        <div><?php echo $val['title'] ?></div>
-                        <div><i class="fas fa-angle-double-down"></i></div>
-                    </div>
-                    <div class="schedule-time">
-                        <?php echo $val['weekdays'] . ' ' . $val['day'] . '-' . $val['month'] . '-' . $val['year'] ?>
-                        &nbsp;&nbsp;
-                        <?php echo _e('Time') . ' : ' . $vTimeStart . ' -- ' . $vTimeEnd ?>
-                    </div>
-                </div>
-                <div class="schedule-content">
-                    <div><label><b><?php _e('Place') ?> :</b></label> <?php echo $val['place'] ?></div>
-                    <div><label><b><?php _e('Note') ?> :</b></label> <?php echo $val['note'] ?></div>
-                </div>
-            </div>
-            <?php
-        } 
+        }
+    
     ?>       
 </div>
 <script type="text/javascript">
     jQuery(document).ready(function () {
         jQuery('.schedule-title').click(function () {
             //dong cac content dang mo
-            jQuery(".schedule-content").css('display', 'none');
+            //jQuery(".schedule-content").css('display', 'none');
             //mo content duoc click
             var contentDisplay = jQuery(this).parent().next(".schedule-content").css('display');
             if (contentDisplay === 'none') {
