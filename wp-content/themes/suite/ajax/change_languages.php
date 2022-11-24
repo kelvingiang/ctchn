116e2fd30ea119b268410f32ebde3ad0d635d3f6
ChiBQ2001 <buiquocchi12tn3@gmail.com>
2022-11-24 11:05:52 +0700

    update style

--- wp-content/themes/suite/ajax/change_languages.php
+++ wp-content/themes/suite/ajax/change_languages.php
@@ -1,15 +1,22 @@
 <?php
 
+use function PHPSTORM_META\type;
+
 define('WP_USE_THEMES', false);
 require('../../../../wp-load.php');
 
-if(isset($_POST['type']) == 'cn'){
+// if(isset($_POST['type']) == 'cn'){
+//     $_SESSION['languages'] = $_POST['type'];
+// }elseif(isset($_POST['type']) == 'vn'){
+//     $_SESSION['languages'] = $_POST['type'];
+// }elseif(isset($_POST['type']) == 'en'){
+//     $_SESSION['languages'] = $_POST['type'];
+// }
+// $respone = array('status' => 'ok'); 
+
+if(isset($_POST)) {
     $_SESSION['languages'] = $_POST['type'];
-}elseif(isset($_POST['type']) == 'vn'){
-    $_SESSION['languages'] = $_POST['type'];
-}elseif(isset($_POST['type']) == 'en'){
-    $_SESSION['languages'] = $_POST['type'];
+    $respone = array('status' => 'ok');  
 }
-$respone = array('status' => 'ok');  
   
 echo json_encode($respone);
