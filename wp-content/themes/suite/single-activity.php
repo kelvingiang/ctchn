3b9bd7381bf9dffbeb5cbeb3e1cc5baa1b529c4e
ChiBQ2001 <buiquocchi12tn3@gmail.com>
2022-12-28 11:55:12 +0700

    update style

--- wp-content/themes/suite/single-activity.php
+++ wp-content/themes/suite/single-activity.php
@@ -2,7 +2,7 @@
 <div class="container-fluid">
     <?php get_template_part('templates/template', 'slider-advertising') ?>
     <div class="row single">
-        <span class="single-head"><?php //echo get_the_date(); ?> | <?php //_e('By'); echo get_the_author() ?></span>
+        <!-- <span class="single-head"><?php //echo get_the_date(); ?> | <?php //_e('By'); echo get_the_author() ?></span> -->
         <div>
             <h2 class="single-title"><?php the_title() ?></h2>
             <div class="single-content"><?php the_content() ?></div>
